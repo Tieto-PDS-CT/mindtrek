59eea3a4210356e42a95b05056cbdf5879d58874
Jani Helin <jani.helin@tieto.com>
2016-10-17 14:30:54 +0300

    Added second debug pane

--- TietoBalloon/src/com/tieto/mindtrek/balloon/MainClass.java
+++ TietoBalloon/src/com/tieto/mindtrek/balloon/MainClass.java
@@ -6,11 +6,12 @@ import java.awt.Dimension;
 import java.awt.event.*;
 
 public class MainClass extends JFrame
-	implements KeyListener, ActionListener {
-	
+    implements KeyListener, ActionListener {
+
 	BalloonController balloonController = null;
 	
-	JTextArea displayArea;
+    JTextArea debug1DisplayArea;
+    JTextArea debug2DisplayArea;
     JTextField typingArea;
     static final String newline = System.getProperty("line.separator");
 
@@ -18,15 +19,10 @@ public class MainClass extends JFrame
     boolean keyDown = false;
     
     public MainClass(String name) {
-		super(name);
-		this.balloonController = new BalloonController();
-	}
+        super(name);
+        this.balloonController = new BalloonController();
+    }
 
-	/**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
     private static void createAndShowGUI() {
         //Create and set up the window.
     	MainClass frame = new MainClass("Tieto Balloon Control");
@@ -34,11 +30,9 @@ public class MainClass extends JFrame
  
         frame.addComponentsToPane();
        
-        
         //Display the window.
         frame.pack();
-        frame.setVisible(true);
-                
+        frame.setVisible(true);    
     }
     
     private void addComponentsToPane() {
@@ -57,13 +51,19 @@ public class MainClass extends JFrame
         //become available to the key event listener.
         //typingArea.setFocusTraversalKeysEnabled(false);
          
-        displayArea = new JTextArea();
-        displayArea.setEditable(false);
-        JScrollPane scrollPane = new JScrollPane(displayArea);
-        scrollPane.setPreferredSize(new Dimension(375, 125));
-         
+        debug1DisplayArea = new JTextArea();
+        debug1DisplayArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(debug1DisplayArea);
+        scrollPane.setPreferredSize(new Dimension(300, 125));
+        
+        debug2DisplayArea = new JTextArea();
+        debug2DisplayArea.setEditable(false);
+        JScrollPane scrollPane2 = new JScrollPane(debug2DisplayArea);
+        scrollPane2.setPreferredSize(new Dimension(800, 125));
+
         getContentPane().add(typingArea, BorderLayout.PAGE_START);
-        getContentPane().add(scrollPane, BorderLayout.CENTER);
+        getContentPane().add(scrollPane, BorderLayout.LINE_START);
+        getContentPane().add(scrollPane2, BorderLayout.LINE_END);
         getContentPane().add(button, BorderLayout.PAGE_END);
     }
     
@@ -72,44 +72,56 @@ public class MainClass extends JFrame
         // displayInfo(e, "KEY TYPED: ");
     }
      
-    public void debugPrint(String message){
+    /**
+     * Prints to the left debug pane on application window, and to console output.
+     * Meant for input keys and other "slow" debug output.
+     */
+    public void debugPrint1(String message){
         System.out.print(message);
-        displayArea.append(message + newline);
+        debug1DisplayArea.append(message + newline);
+    }
+    
+    /**
+     * Prints to the rigth (wider) debug pane on the application window, and to console output.
+     * Meant for MQTT traffic and other "fast" debug output.
+     */
+    public void debugPrint2(String message){
+        System.out.print(message);
+        debug2DisplayArea.append(message + newline);
     }
     
     /** Handle the key pressed event from the text field. */
     public void keyPressed(KeyEvent e) {
-    	if (!keyDown){
-    		keyDown = true;
-    		// displayInfo(e, "KEY PRESSED: ");
-    		
-    		if (e.getKeyCode() == KeyEvent.VK_UP){
-    			this.balloonController.goForward();
-    			debugPrint("Forward!");
-    		} else if (e.getKeyCode() == KeyEvent.VK_DOWN){
-    			this.balloonController.goBackward();
-    			debugPrint("Backward!");
-    		} else if (e.getKeyCode() == KeyEvent.VK_LEFT){
-    			this.balloonController.turnLeft();
-    			debugPrint("Turn left!");
-    		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
-    			this.balloonController.turnRight();
-    			debugPrint("Turn right!");
-    		} else if (e.getKeyCode() == KeyEvent.VK_1){
-    			this.balloonController.goUp(1);
-    			debugPrint("1 Step more height!");
-			} else if (e.getKeyCode() == KeyEvent.VK_2){
-    			this.balloonController.goUp(2);
-				debugPrint("2 Steps more height!");
-			} else if (e.getKeyCode() == KeyEvent.VK_3){
-    			this.balloonController.goUp(3);
-				debugPrint("3 Steps more height!");
-			} else if (e.getKeyCode() == KeyEvent.VK_4){
-    			this.balloonController.goUp(4);
-				debugPrint("4 Steps more height!");
-			}
-		}
-	}
+        if (!keyDown){
+            keyDown = true;
+    
+            if (e.getKeyCode() == KeyEvent.VK_UP){
+                this.balloonController.goForward();
+                debugPrint1("Forward!");
+            } else if (e.getKeyCode() == KeyEvent.VK_DOWN){
+                this.balloonController.goBackward();
+                debugPrint1("Backward!");
+            } else if (e.getKeyCode() == KeyEvent.VK_LEFT){
+                this.balloonController.turnLeft();
+                debugPrint1("Turn left!");
+            } else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+                this.balloonController.turnRight();
+                debugPrint1("Turn right!");
+            } else if (e.getKeyCode() == KeyEvent.VK_1){
+                this.balloonController.goUp(1);
+                debugPrint1("1 Step more height!");
+            } else if (e.getKeyCode() == KeyEvent.VK_2){
+                this.balloonController.goUp(2);
+                debugPrint1("2 Steps more height!");
+            } else if (e.getKeyCode() == KeyEvent.VK_3){
+                this.balloonController.goUp(3);
+                debugPrint1("3 Steps more height!");
+            } else if (e.getKeyCode() == KeyEvent.VK_4){
+                this.balloonController.goUp(4);
+                debugPrint1("4 Steps more height!");
+            }
+        }
+     }
     
      
     /** Handle the key released event from the text field. */
@@ -121,7 +133,8 @@ public class MainClass extends JFrame
     /** Handle the button click. */
     public void actionPerformed(ActionEvent e) {
         //Clear the text components.
-        displayArea.setText("");
+        debug1DisplayArea.setText("");
+        debug2DisplayArea.setText("");
         typingArea.setText("");
          
         //Return the focus to the typing area.
@@ -175,26 +188,20 @@ public class MainClass extends JFrame
             locationString += "unknown";
         }
          
-        displayArea.append(keyStatus + newline
+        debug1DisplayArea.append(keyStatus + newline
                 + "    " + keyString + newline
                 + "    " + modString + newline
                 + "    " + actionString + newline
                 + "    " + locationString + newline);
-        displayArea.setCaretPosition(displayArea.getDocument().getLength());
+        debug1DisplayArea.setCaretPosition(debug1DisplayArea.getDocument().getLength());
     }
  
     public static void main(String[] args) {
-    	
-    	System.out.print("Starting balloon program");
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
             }
         });
-    	System.out.print("Exiting balloon program");
-
     }
 }
 
